7ed8b5d17f7b41a7715c831672bb17588baa6722
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-01-06 00:16:58 +0200

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,16 @@
 import css from './Profile/Profile.module.css';
 import PropTypes from 'prop-types';
 
-export const  Profile = ({ username, tag, location, avatar, stats }) = > {
+export const  Profile = ({ username, tag, location, avatar, stats }) => {
     return(
-    
+        <div className={css.profile}>
+            <div className={css.description}>
+                <img src={avatar} alt={username} width="300" className={css.avatar} />
+            <p></p>
+            <p></p>
+            <p></p>
+            </div>
+    </div >
     );
 };
 
