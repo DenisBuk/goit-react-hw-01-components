41847b1fc5046ed4e77986bde2d0452ac9a829cf
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-01-06 00:49:29 +0200

    profil.module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+
 import user from '../path/user.json';
 import data from '../path/data.json';
 import friends from '../path/friends.json';
 import transactions from '../path/transactions.json';
 
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
