b69db31a533e28aee7a1250bc7dcc3b0e836d3d6
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-01-06 16:56:27 +0200

    FriendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,7 @@ import css from './FriendList.module.css';
 import FriendListItem from '../FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 
-export const FriendList = ({ friends }) => {
+export  const FriendList = ({ friends }) => {
     return (
         <div className={css.friendList}>
             <ul key={friends.id}>
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,4 +1,18 @@
 import css from './FriendListItem.module.css';
 import PropTypes from 'prop-types';
 
-export
+export default function FriendListItem ({ avatar, name, isOnline }) {
+    return (
+        <li className={css.item}>
+            <span className={isOnline ? css.isLine : css.ofLine}></span>
+            <img className={css.avatar} src={avatar} alt="User avatar"  width="48"/>
+            <p className={css.name}>{name}</p>
+        </li>
+    );
+}
+ 
+FriendListItem.prototype = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+};
